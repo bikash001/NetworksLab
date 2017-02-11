20c18a7f4d9fd83feb9b81eaa6532e0fb0a344e8
Bikash Gogoi <bikashgogoi001@gmail.com>
2017-02-11 18:06:10 +0530

    renamed folder

--- LAB4/Server.java
+++ LAB4/Server.java
@@ -4,20 +4,21 @@ import java.lang.Integer;
 
 public class Server {
 	public static void main(String[] args) {
+		//check if arguments are correct
 		if (args.length < 1) {
 			System.out.println("Usage: <port>");
 		}
-		int port = Integer.valueOf(args[0]);
-		int maxConn = 10;
+		int port = Integer.valueOf(args[0]);	//port no.
+		int maxConn = 10;	//maximum connection
 
-		ServerSocket serverSocket = null;
-        Socket socket = null;
+		ServerSocket serverSocket = null;	//server socket
+        Socket socket = null;	//client socket
 
         try{
-        	serverSocket = new ServerSocket(port, maxConn);
+        	serverSocket = new ServerSocket(port, maxConn); //create server socket
 	        while (true) {
-	            socket = serverSocket.accept();
-	            new EmailThread(socket).start();
+	            socket = serverSocket.accept();	//listen to new connection
+	            new EmailThread(socket).start();	//create new thread to serve the connection
 	        }
 	    } catch (IOException e) {
 	    	e.printStackTrace();
@@ -26,62 +27,87 @@ public class Server {
 }
 
 class EmailThread extends Thread {
-    private Socket socket;
+    private Socket socket;	//client socket
 
     public EmailThread(Socket clientSocket) {
         this.socket = clientSocket;
     }
 
     public void run() {
-        DataInputStream in = null;
-        DataOutputStream out = null;
+        DataInputStream in = null;	//input buffer
+        DataOutputStream out = null;	//output buffer
         System.out.println("Connection established");
         String line, currUserName = null;
+
         try {
-	        in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            File currUserFile = null;
-            RandomAccessFile raf = null;
+	        in = new DataInputStream(socket.getInputStream());	//get input buffer
+            out = new DataOutputStream(socket.getOutputStream());	//get output buffer
+            
+            File currUserFile = null;	//current user spool file
+            RandomAccessFile raf = null;	//handler for read write from file
+        	
         	while (true) {
-                line = in.readUTF();
+                line = in.readUTF();	//get command
                 System.out.println(line);
                 String[] cmds = line.split(" ");
+                
+                //if something went wrong
                 if (line == null) {
                 	break;
                 } else {
+                	// command LSTU
                     if (cmds[0].equals("LSTU")) {
-                    	File folder = new File("Spool");
-						File[] listOfFiles = folder.listFiles();
+                    	File folder = new File("Spool");	//get Spool folder
+						File[] listOfFiles = folder.listFiles();	//list of files in the spool folder
 						StringBuilder builder = new StringBuilder();
                         String uname;
+
+                        // get the list of users
 					    for (File entry : listOfFiles) {
                             uname = entry.getName();
 					      builder.append(uname.substring(0,uname.length()-4)).append("\n");
 					    }
+
+					    //if no user available
                         if (listOfFiles.length == 0) {
                             builder.append("No users available.\n");
                         }
-                    	out.writeUTF(builder.toString());
+                    	out.writeUTF(builder.toString());	//send response
                     	out.writeUTF("###");
-                    } else if (cmds[0].equals("ADDU")) {
-                    	File file = new File("Spool/"+cmds[1]+".dat");
+                    }
+
+                    //command ADDU
+                    else if (cmds[0].equals("ADDU")) {
+                    	File file = new File("Spool/"+cmds[1]+".dat");	//get spool file for the user
+                    	
+                    	//check if user exist
                     	if (file.exists()) {
                     		out.writeUTF("Userid already present.\n");
        						out.writeUTF("###");
-                    	} else {
+                    	}
+                    	// create new spool file, if user does not exist
+                    	else {
                     		file.createNewFile();
        						out.writeUTF("User created successfully.\n");
        						out.writeUTF("###");
                     	}
-                    } else if (cmds[0].equals("USER")) {
-                    	currUserFile = new File("Spool/"+cmds[1]+".dat");
+                    }
+
+                    // command USER
+                    else if (cmds[0].equals("USER")) {
+                    	currUserFile = new File("Spool/"+cmds[1]+".dat");	//get current user spool file
+                    	
+                    	// if user file exist
                     	if (currUserFile.exists()) {
                             currUserName = cmds[1];
-                    		raf = new RandomAccessFile(currUserFile,"r");
+                    		raf = new RandomAccessFile(currUserFile,"r");	//open file for reading
                             String ln;
                     		int count = 0;
+                    		// check if the file contains any message
                     		if (raf.length() > 0) {
                     			ln = raf.readLine();
+
+                    			// check how many messages are there
 	                    		while(ln != null) 	{
 	                    			if (ln.equals("###")) {
 	                    				count++;
@@ -92,14 +118,22 @@ class EmailThread extends Thread {
                     		raf.seek(0);
                     		out.writeUTF("Welcome to your mailbox.\nThere are "+count+" messages in your mailbox.\n");
        						out.writeUTF("###");
-                    	} else {
+                    	}
+                    	// user file does not exist
+                    	else {
                             System.out.println(cmds[1]+" does not exist.");
                     		out.writeUTF("User does not exist.\n");
                     		out.writeUTF("###");
                     	}
-                    } else if (cmds[0].equals("READM")) {
+                    }
+
+                    // command READM
+                    else if (cmds[0].equals("READM")) {
+                    	// check if the user already in sub-prompt
                         if (raf != null && currUserFile != null) {
                             String ln = raf.readLine();
+
+                            // get the message from the spool file, if the file pointer points to any message
                     		if (ln != null) {
                     			StringBuilder bd = new StringBuilder();
                     			while(!ln.equals("###")) {
@@ -108,29 +142,48 @@ class EmailThread extends Thread {
                     			}
                     			out.writeUTF(bd.toString());
                                 out.writeUTF("###");
-                    		} else {
+                    		}
+                    		// no messages, or file pointer at the end
+                    		else {
                     			out.writeUTF("No More Mail\n");
                     			out.writeUTF("###");
                     		}
-                    	} else {
+                    	}
+                    	// SetUser command required first
+                    	else {
                     		out.writeUTF("Error: current user not set.\n");
                     		out.writeUTF("###");
                     	}
-                    } else if (cmds[0].equals("DELM")) {
+                    }
+
+                    // command DELM
+                    else if (cmds[0].equals("DELM")) {
+                    	// check if current user is set
                     	if (raf != null && currUserFile != null) {
-                    		long pos = raf.getFilePointer();
+                    		long pos = raf.getFilePointer();	//get current file pointer
                             String ln = raf.readLine();
+
+                            // check if end of file encountered
                             if (ln != null) {
                                 long epos = 0;
+
+                                //skip the message to be deleted
                     			while(!ln.equals("###")) {
                     				ln = raf.readLine();
                     			}
-                                epos = raf.getFilePointer();
+                                epos = raf.getFilePointer();	//file pointer after the message to be deleted
                                 raf.seek(0);
+
+                                //temporary file
                                 File tempFile = new File(cmds[1]+".tmp");
                                 tempFile.createNewFile();
+
                                 byte[] buf;
                                 FileOutputStream fs = new FileOutputStream(tempFile);
+                                
+                                // copy the content of the user's spool file
+                                // to temporary file till the message before the message
+                                // to be deleted
                                 if (pos > Integer.MAX_VALUE) {
                                     long diff = pos;
                                     buf = new byte[Integer.MAX_VALUE];
@@ -149,8 +202,13 @@ class EmailThread extends Thread {
                                     raf.readFully(buf);
                                     fs.write(buf);
                                 }
+
+
                                 raf.seek(epos);
                                 long len = raf.length() - epos;
+
+                                // copy the rest of the messages after the message
+                                // to be deleted
                                 if (len > 0) {
                                     if (len > Integer.MAX_VALUE) {
                                         buf = new byte[Integer.MAX_VALUE];
@@ -171,27 +229,36 @@ class EmailThread extends Thread {
                                     }
                                 }
                                 raf.close();
-                                currUserFile.delete();
+                                
+                                currUserFile.delete();	// delete users spool file
                                 fs.close();
-                                tempFile.renameTo(currUserFile);
+                                tempFile.renameTo(currUserFile); // rename temp file to user's spool file
                                 currUserFile = tempFile;
                                 raf = new RandomAccessFile(currUserFile,"r");
-                                raf.seek(pos);
-                    			out.writeUTF("Message Deleted\n");
+                                raf.seek(pos); 	// set the file pointer
+                    			out.writeUTF("Message Deleted\n");	// send the response
                                 out.writeUTF("###");
-                    		} else {
+                    		} else {	// file pointer at the end
                     			out.writeUTF("No More Mail\n");
                     			out.writeUTF("###");
                     		}
-                    	} else {
+                    	} else {	// user not set
                     		out.writeUTF("Error: current user not set.\n");
                     		out.writeUTF("###");
                     	}
-                    } else if (cmds[0].equals("SEND")) {
+                    }
+
+                    // command SEND
+                    else if (cmds[0].equals("SEND")) {
+                    	// check if user is set
                     	if (currUserFile != null && in != null) {
                     		String str = in.readUTF();
-                    		File toFile = new File("Spool/"+cmds[1]+".dat");
+                    		File toFile = new File("Spool/"+cmds[1]+".dat");	// get spool file of the user to whom message to be sent
+                    		
+                    		// check if user exist
                     		if (toFile.exists()) {
+
+                    			// append the message at the end of the user's spool file
                     			BufferedWriter tbr = new BufferedWriter(new FileWriter(toFile));
                     			tbr.append("From: "+currUserName+"\n");
                     			tbr.append("To: "+cmds[1]+"\n");
@@ -200,16 +267,21 @@ class EmailThread extends Thread {
                                 tbr.close();
                                 out.writeUTF("Message sent successfully.");
                                 out.writeUTF("###");
-                    		} else {
+                    		} else {	// user not exist
                     			out.writeUTF("User does not exist.\n");
                     			out.writeUTF("###");
                     		}
-                    	} else {
+                    	}
+                    	// user not set
+                    	else {
                             out.writeUTF("Error: current user not set.\n");
                             out.writeUTF("###");
                     		System.out.println("current user does not exist.\n");
                     	}
-                    } else if (cmds[0].equals("DONEU")) {
+                    }
+
+                    // command DONEU
+                    else if (cmds[0].equals("DONEU")) {
                     	out.writeUTF("User closed successfully.\n");
                         out.writeUTF("###");
                         currUserFile = null;
@@ -218,11 +290,17 @@ class EmailThread extends Thread {
                     		raf.close();
                     		raf = null;
                     	}
-                    } else if (cmds[0].equals("QUIT")) {
+                    }
+
+                    // if command QUIT, break out of the while loop
+                    else if (cmds[0].equals("QUIT")) {
                         out.writeUTF("Closed Connection with the server.");
                         out.writeUTF("###");
                     	break;
-                    } else {
+                    }
+
+                    // invalid command
+                    else {
                         System.out.println("invalid command.");
                     	out.writeUTF("INVALID COMMAND\n");
                     	out.writeUTF("###");
@@ -233,6 +311,7 @@ class EmailThread extends Thread {
 	    	e.printStackTrace();
 	    } finally {
 	    	try {
+	    		// close the resources
 	    		if (in != null) {
 	    			in.close();
 	    		}
