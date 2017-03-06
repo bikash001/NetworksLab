b568fecc1067e965b13d20532fded0c9deee5024
Bikash Gogoi <bikashgogoi001@gmail.com>
2017-03-06 17:28:48 +0530

    all functionality implemented

--- Lab6/Receiver.java
+++ Lab6/Receiver.java
@@ -9,7 +9,7 @@ public class Receiver {
             int len = args.length;
             int port = 1080;
             int maxPkts = 10;
-            double perror = 0.00001;
+            double perror = 0.001;	//packet drop probability;
             boolean debugMode = false;
             int maxSeqNo = 255;
 
@@ -29,6 +29,7 @@ public class Receiver {
             int NFE = 0;
             try {
                 DatagramSocket ds = new DatagramSocket(port);
+                long startTime = System.currentTimeMillis();
                 byte[] buf = new byte[1024];
                 for (int count=0; count < maxPkts;) {
                     DatagramPacket dp = new DatagramPacket(buf,1024);
@@ -37,16 +38,40 @@ public class Receiver {
                     	continue;
                     }
                     
-                    String str = new String(dp.getData(),0,dp.getLength());
+                    // String str = new String(dp.getData(),0,dp.getLength());
                     // System.out.println("Received: "+str);
-                    if (Integer.valueOf(str.substring(0,str.indexOf(' '))) == NFE) {
+                    int seq = 0;
+					seq = buf[0];
+					seq = (seq << 8) + buf[1];
+					seq += (seq << 8) + buf[2];
+					seq += (seq << 8) + buf[3];
+					// System.out.printf("%x",buf[0]);
+					// System.out.printf("%x",buf[1]);
+					// System.out.printf("%x",buf[2]);
+					// System.out.printf("%x",buf[3]);
+					System.out.printf("\nrecieved %d\n", seq);
+					if (debugMode) {
+						System.out.printf("%d:\t Time Received: %d:00  Packet droped: false\n",seq,System.currentTimeMillis()-startTime);
+					}
+
+                    if (seq == NFE) {
                     	NFE++;
                     }
-                    if (NFE == maxSeqNo) {
+                    if (NFE == Integer.MAX_VALUE) {
                     	NFE = 0;
                     }
                     String result = "ACK "+NFE;
-                    DatagramPacket dpSend = new DatagramPacket(result.getBytes(),result.length(),dp.getSocketAddress());
+                    byte[] arr = new byte[1024];
+                    arr[0] = (byte)(NFE >> 24);
+					arr[1] = (byte)(NFE >> 16);
+					arr[2] = (byte)(NFE >> 8);
+					arr[3] = (byte)(NFE);
+					// System.out.printf("%x",arr[0]);
+					// System.out.printf("%x",arr[1]);
+					// System.out.printf("%x",arr[2]);
+					// System.out.printf("%x",arr[3]);
+					// System.out.println("");
+                    DatagramPacket dpSend = new DatagramPacket(arr,arr.length,dp.getSocketAddress());
                     ds.send(dpSend);
                     ++count;
                 }
--- Lab6/Sender.java
+++ Lab6/Sender.java
@@ -2,70 +2,274 @@ import java.net.*;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.lang.Thread;
+import java.util.TimerTask;
+import java.util.Random;
+import java.util.Timer;
+import java.util.HashMap;
+import java.lang.System;
+import java.util.Iterator;
 
 public class Sender {
-  public static void main(String[] args) {
-    boolean debug = false;
-    int port = 1080;
-    int pktLen = 100;
-    int pktGenRate = 10;
-    int maxPkts = 1000;
-    int maxBuffSize = 100;
+	public static int toAck = 0;
+	public static int lastSent = 0;
+	public static HashMap<Integer,Timeout> timerList;
+	public static HashMap<Integer,Integer> resends;
+	public static long rtt = 0;
+	public static long totalAcked;
+	public static int maxPkts = 100;
+	public static DatagramSocket ds = null;
+	public static Timer timer = null;
+	public static int totalTrans = 0;
+	public static int pktGenRate = 1;
+	public static int pktLen = 100;
+	public static boolean debug = false;
+	public static long startTime;
+	    
+  	public static void main(String[] args) {
+	    int port = 1080;
+	    int maxBuffSize = 100;
+	    int wsize = 10;
+	    InetAddress ip = null;
+	    timerList = new HashMap<Integer,Timeout>();
+	    try {
+		    for (int i=0; i<args.length; ++i) {
+		    	if (args[i].equals("-d")) {
+		    		debug = true;
+		    	} else if (args[i].equals("-s")) {
+		    		ip = InetAddress.getByName(args[++i]);
+		    	} else if (args[i].equals("-p")) {
+		    		port = Integer.valueOf(args[++i]);
+		    	} else if (args[i].equals("-l")) {
+		    		pktLen = Integer.valueOf(args[++i]);
+		    	} else if (args[i].equals("-r")) {
+		    		pktGenRate = Integer.valueOf(args[++i]);
+		    	} else if (args[i].equals("-n")) {
+		    		maxPkts = Integer.valueOf(args[++i]);
+		    	} else if (args[i].equals("-w")) {
+		    		wsize = Integer.valueOf(args[++i]);
+		    	} else if (args[i].equals("-b")) {
+		    		maxBuffSize = Integer.valueOf(args[++i]);
+		    	}
+		    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    for (int i=0; i<args.length; ++i) {
-    	if (args[i].equals("-d")) {
-    		debug = true;
-    	} else if (args[i].equals("-s")) {
-    		ip = InetAddress.getByName(args[++i]);
-    	} else if (args[i].equals("-p")) {
-    		port = Integer.valueOf(args[++i]);
-    	} else if (args[i].equals("-l")) {
-    		pktLen = Integer.valueOf(args[++i]);
-    	} else if (args[i].equals("-r")) {
-    		pktGenRate = Integer.valueOf(args[++i]);
-    	} else if (args[i].equals("-n")) {
-    		maxPkts = Integer.valueOf(args[++i]);
-    	} else if (args[i].equals("-b")) {
-    		maxBuffSize = Integer.valueOf(args[++i]);
-    	}
-    }
+	    ConcurrentLinkedQueue<byte[]> queBuffer = new ConcurrentLinkedQueue<byte[]>();
+	    timer = new Timer();
+	    resends = new HashMap<Integer,Integer>();
+	    PacketGen generator = new PacketGen(queBuffer, pktLen, maxBuffSize);
+	    timer.schedule(generator,0,1000/pktGenRate);
+	   	
+	    try {
+			ds = new DatagramSocket();
+			startTime = System.currentTimeMillis();
+			byte[] buff = null;
+			Recv receiver = new Recv(ds,timerList,resends,queBuffer);
+			receiver.start();
+	    	int pktNo = 0;
+	    	Iterator<byte[]> itr = queBuffer.iterator();
+	    	for (int i=0; i < 10; ) {
+	        		
+	        	if (itr.hasNext() && (lastSent-toAck+1) < wsize) {
+	        		System.out.println("has");
+	        		i++;
+		        	buff = itr.next();
+		        	DatagramPacket dpSend = new DatagramPacket(buff,buff.length,ip,port);
+		        	ds.send(dpSend);
+		        	Timeout tout = new Timeout(timerList, ++lastSent, resends,dpSend);
+		        	timerList.put(lastSent,tout);
+		        	timer.schedule(tout, 100);
+		        	// System.out.println(buff);
+		        	resends.put(pktNo, 0);
+		        	if (pktNo == Integer.MAX_VALUE) {
+		        		pktNo = 0;
+		        	} else {
+		        		pktNo++;
+		        	}
+		        	totalTrans++;
+		        }
+			}
+			while (totalAcked < maxPkts) {
+				if (itr.hasNext() && lastSent-toAck+1 < wsize) {
+					buff = itr.next();
+		        	DatagramPacket dpSend = new DatagramPacket(buff,buff.length,ip,port);
+		        	ds.send(dpSend);
+		        	Timeout tout = new Timeout(timerList, ++lastSent, resends, dpSend);
+		        	timerList.put(lastSent,tout);
+		        	timer.schedule(tout, 2 * rtt);
+		        	resends.put(pktNo,0);
+		        	if (pktNo == Integer.MAX_VALUE) {
+		        		pktNo = 0;
+		        	} else {
+		        		pktNo++;
+		        	}
+		        	totalTrans++;
+				}
+			}
+	      
+	    } catch(Exception e){
+	    	e.printStackTrace();
+	    }
+	}
 
-    ConcurrentLinkedQueue<byte[]> queBuffer = new ConcurrentLinkedQueue();
-
-
-    try {
-      DatagramSocket ds = new DatagramSocket();
-      InetAddress ip = InetAddress.getByName(args[0]);
-      int port = Integer.valueOf(args[1]);
-      String str;
-      byte[] buf = new byte[1024];
-      Scanner in = new Scanner(System.in);
-      System.out.print("Enter command: ");
-      str = in.nextLine();
-      do {
-        DatagramPacket dpSend = new DatagramPacket(str.getBytes(),str.length(),ip,port);
-        ds.send(dpSend);
-        DatagramPacket dpRecv = new DatagramPacket(buf,1024); 
-        ds.receive(dpRecv);
-        String msg = new String(dpRecv.getData(),0,dpRecv.getLength());
-        System.out.println("Answer from server: "+msg);
-        System.out.print("Enter command: ");
-        str = in.nextLine();
-      } while (!str.isEmpty());
-      
-    } catch(Exception e){
-    	System.out.println(e.toString());
-    }
-  }
+	public static void finalStatus() {
+		System.out.printf("Packet Generation Rate: %d\n", pktGenRate);
+		System.out.printf("Packet Length: %d\n", pktLen);
+		System.out.printf("Retransmission Ration: %f\n", (float)totalTrans/totalAcked);
+		System.out.printf("Average RTT: %d ms\n", rtt);
+	}
 }
 
-class PacketGen extends Thread {
+class Recv extends Thread {
+	private DatagramSocket ds;
+	private HashMap<Integer,Timeout> tasks;
+	private HashMap<Integer,Integer> resends;
 	private ConcurrentLinkedQueue<byte[]> queBuffer;
-	public PacketGen(ConcurrentLinkedQueue<byte[]> buff) {
-		queBuffer = buff;
+	public Recv(DatagramSocket soc, HashMap<Integer,Timeout> ts,HashMap<Integer,Integer> rs, ConcurrentLinkedQueue<byte[]> buf) {
+		ds = soc;
+		tasks = ts;
+		resends = rs;
+		queBuffer = buf;
 	}
 
 	public void run() {
-		
+		byte[] buff = new byte[1024];
+		long timediff;
+		long totalPktAcked = 0;
+		try {
+			while (true) {
+				DatagramPacket dpRecv = new DatagramPacket(buff,1024); 
+				ds.receive(dpRecv);
+				int seq = 0;
+				seq = buff[0];
+				seq = (seq << 8) + buff[1];
+				seq += (seq << 8) + buff[2];
+				seq += (seq << 8) + buff[3];
+				// System.out.printf("%x",buff[0]);
+				// System.out.printf("%x",buff[1]);
+				// System.out.printf("%x",buff[2]);
+				// System.out.printf("%x",buff[3]);
+				System.out.printf("\nreceived %d\n", seq);
+				if (Sender.debug) {
+					if (tasks.containsKey(Sender.toAck)) {
+						timediff = tasks.get(Sender.toAck).getRtt();
+						Sender.rtt = ((Sender.rtt * totalPktAcked) + timediff) / (totalPktAcked + 1);
+						System.out.printf("%d:\t Time Generated: %d:00 RTT: %d\t Number of Attempts: %d\n",seq,tasks.get(Sender.toAck).genTime(),timediff,resends.get(Sender.toAck)+1);
+						totalPktAcked++;
+						tasks.remove(Sender.toAck);
+					}
+					resends.remove(Sender.toAck);
+					queBuffer.poll();
+					Sender.totalAcked++;
+					Sender.toAck++;
+				}
+
+				for (; Sender.toAck < seq; Sender.toAck++) {
+					if (tasks.containsKey(Sender.toAck)) {
+						timediff = tasks.get(Sender.toAck).getRtt();
+						Sender.rtt = ((Sender.rtt * totalPktAcked) + timediff) / (totalPktAcked + 1);
+						totalPktAcked++;
+						tasks.remove(Sender.toAck);
+					}
+					resends.remove(Sender.toAck);
+					queBuffer.poll();
+					Sender.totalAcked++;
+				}
+				if (totalPktAcked >= Sender.maxPkts) {
+					Sender.finalStatus();
+					System.exit(0);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class PacketGen extends TimerTask {
+	private ConcurrentLinkedQueue<byte[]> queBuffer;
+	private Random rand;
+	private int length;
+	private int seqNo = 0;
+	private int buffSize;
+	public PacketGen(ConcurrentLinkedQueue<byte[]> buff, int len, int maxBuff) {
+		queBuffer = buff;
+		rand = new Random();
+		length = len;
+		buffSize = maxBuff;
+	}
+
+	@Override
+	public void run() {
+		if (queBuffer.size() < buffSize) {
+			byte[] arr = new byte[length];
+			rand.nextBytes(arr);
+			arr[0] = (byte)(seqNo >> 24);
+			arr[1] = (byte)(seqNo >> 16);
+			arr[2] = (byte)(seqNo >> 8);
+			arr[3] = (byte)(seqNo);
+			queBuffer.add(arr);
+			if (seqNo == Integer.MAX_VALUE) {
+				seqNo = 0;
+			} else {
+				++seqNo;
+			}
+		}
+	}
+}
+
+class Timeout extends TimerTask {
+	private int seq;
+	private HashMap<Integer,Timeout> tasks;
+	public long sendTime;
+	private HashMap<Integer,Integer> resends;
+	private DatagramPacket dp;
+	public Timeout(HashMap<Integer,Timeout> timerList, int sn, HashMap<Integer,Integer> rs, DatagramPacket ds) {
+		tasks = timerList;
+		seq = sn;
+		resends = rs;
+		dp = ds;
+		sendTime = System.currentTimeMillis();
+	}
+
+	@Override
+	public void run() {
+		for (int i=Sender.toAck; i <= seq; ++i) {
+			if (tasks.containsKey(i)) {
+				tasks.get(i).resend(i);
+				tasks.remove(i);
+			}
+		}
+	}
+
+	public void resend(Integer i) {
+		this.cancel();
+		if (resends.containsKey(i)) {
+			Integer val = resends.get(i);
+			if (val >= 5) {
+				Sender.finalStatus();
+				System.exit(0);
+			}
+			val++;
+			try {		
+				Sender.ds.send(dp);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Timeout ts = new Timeout(tasks,seq,resends,dp); 
+			Sender.timer.schedule(ts,Sender.rtt * 2);
+			tasks.put(i,ts);
+			Sender.totalTrans++;
+		}
+	}
+
+	public long genTime() {
+		return sendTime - Sender.startTime;
+	}
+
+	public long getRtt() {
+		this.cancel();
+		return System.currentTimeMillis() - sendTime;
 	}
 }
